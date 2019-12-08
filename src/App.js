1d7bfd630367bbabbcd1452ebbc4f705ce9fe0b6
ealili <enesalili00@gmail.com>
2019-12-08 11:54:22 +0100

    cleanup

--- src/App.js
+++ src/App.js
@@ -1,38 +1,38 @@
-import React, { Component } from "react";
-import "./App.css";
-import Home from "./components/pages/homepage/Home";
-import { Switch, Route, BrowserRouter as Router } from "react-router-dom";
-import NotFound from "./components/pages/NotFound";
-import Header from "./components/header/Header";
-import PhonesRoutes from "./components/PhonesRoutes";
-import About from "./components/pages/about/About";
-import Footer from "./components/footer/Footer";
-import AddPhone from './components/admin/AddPhone';
-import Admin  from './components/admin/Admin';
-import Login from './components/pages/login/login.jsx';
+import React, { Component } from 'react'
+import './App.css'
+import Home from './components/pages/homepage/Home'
+import { Switch, Route, BrowserRouter as Router } from 'react-router-dom'
+import NotFound from './components/pages/NotFound'
+import Header from './components/header/Header'
+import PhonesRoutes from './components/PhonesRoutes'
+import About from './components/pages/about/About'
+import Footer from './components/footer/Footer'
+import AddPhone from './components/admin/AddPhone'
+import Admin from './components/admin/Admin'
+import Login from './components/pages/login/Login'
 
 class App extends Component {
-  render() {
+  render () {
     return (
-        <Router>
-          <React.Fragment>
-            <Header />
-            <section id="mainContent">
-              <Switch>
-                <Route exact path="/" component={Home} />
-                <Route path="/phones" component={PhonesRoutes} />
-                <Route path="/about" component={About} />
-                <Route path="/admin/addphone" component={AddPhone} />
-                <Route path="/admin" component={Admin}/>
-                <Route path="/login" component={Login}/>
-                <Route component={NotFound} />
-              </Switch>
-            </section>
-            <Footer />
-          </React.Fragment>
-        </Router>
-    );
+      <Router>
+        <React.Fragment>
+          <Header/>
+          <section id="mainContent">
+            <Switch>
+              <Route exact path="/" component={Home}/>
+              <Route path="/phones" component={PhonesRoutes}/>
+              <Route path="/about" component={About}/>
+              <Route path="/admin/addphone" component={AddPhone}/>
+              <Route path="/admin" component={Admin}/>
+              <Route path="/login" component={Login}/>
+              <Route component={NotFound}/>
+            </Switch>
+          </section>
+          <Footer/>
+        </React.Fragment>
+      </Router>
+    )
   }
 }
 
-export default App;
+export default App
--- src/components/pages/homepage/Home.jsx
+++ src/components/pages/homepage/Home.jsx
@@ -1,14 +1,14 @@
-import React from "react";
-import Carousel from "../../carousel/Carousel";
-import LatestPhones from "../LatestPhones";
+import React from 'react'
+import Carousel from '../../carousel/Carousel'
+import LatestPhones from '../LatestPhones'
 
-export default function Home() {
+export default function Home () {
   return (
     <div>
-      <Carousel />
-      <div className="container" style={{ paddingTop: "2%" }}>
-        <LatestPhones />
+      <Carousel/>
+      <div className="container" style={{ paddingTop: '2%' }}>
+        <LatestPhones/>
       </div>
     </div>
-  );
+  )
 }

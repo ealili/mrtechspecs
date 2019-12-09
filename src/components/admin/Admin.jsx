6a4afd2afebb394824cc6a0ba1039d757d5f7de1
Your Name <a.selaudin@gmail.com>
2019-12-09 18:22:00 +0100

    Login feature added

--- src/components/admin/Admin.jsx
+++ src/components/admin/Admin.jsx
@@ -1,23 +1,49 @@
-import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
+import React, {Component} from 'react'
+import {Link, Redirect} from 'react-router-dom'
 
 class Admin extends Component {
-  render () {
-    return (
-      <div>
-        <div className="col-lg-3 col-md-4 col-xs-6">
-          <Link to="/admin/addphone" className="nav-link">
-            Add phone
-          </Link>
-        </div>
-        <div className="col-lg-3 col-md-4 col-xs-6">
-          <Link to="/modify" className="nav-link">
-            Modify
-          </Link>
-        </div>
-      </div>
-    )
-  }
+    state = {
+        redirect: false,
+        user: {}
+    }
+
+    componentWillMount() {
+        if (sessionStorage.getItem('userData')) {
+            console.log(sessionStorage.getItem('userData'))
+        } else {
+            this.setState({redirect: true});
+        }
+    }
+
+    logout(){
+        sessionStorage.setItem("userData",'');
+        sessionStorage.clear();
+        this.setState({redirect: true})
+        console.log(sessionStorage.getItem('userData'))
+    }
+
+    render() {
+        if (this.state.redirect) {
+            return (<Redirect to={'/login'}/>)
+        }
+        return (
+            <div>
+                <div className="col-lg-3 col-md-4 col-xs-6">
+                    <Link to="/admin/addphone" className="nav-link">
+                        Add phone
+                    </Link>
+                </div>
+                <div className="col-lg-3 col-md-4 col-xs-6">
+                    <Link to="/modify" className="nav-link">
+                        Modify
+                    </Link>
+                </div>
+                <div className="col-lg-3 col-md-4 col-xs-6">
+                    <a href='' onClick={this.logout.bind(this)} className="logout nav-link">Logout</a>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default Admin
--- src/components/pages/login/Login.jsx
+++ src/components/pages/login/Login.jsx
@@ -1,75 +1,106 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 import "./css/main.css";
 import "./css/util.css";
 import Tilt from "react-tilt";
+import Redirect from "react-router-dom/Redirect";
 
 export default class Login extends Component {
-  render() {
-    return (
-      <React.Fragment>
-        <div className="limiter">
-          <div className="container-login100">
-            <div className="wrap-login100">
-              <Tilt className="Tilt" options={{ scale: 1.0 }}>
-                <div className="login100-pic js-tilt" data-tilt>
-                  <img src={require("./images/img-01.png")} alt="IMG" />
-                </div>
-              </Tilt>
-              <form className="login100-form">
-                <span className="login100-form-title">Member Login</span>
+    state = {
+        username: "",
+        password: "",
+        redirect: false,
+        error: null
+    }
 
-                <div className="wrap-input100">
-                  <input
-                    className="input100"
-                    type="text"
-                    name="email"
-                    placeholder="Email"
-                  />
-                  <span className="focus-input100"></span>
-                  <span className="symbol-input100">
-                    <i className="fa fa-envelope" aria-hidden="true" />
+    handleChange(e) {
+        this.setState({[e.target.name]: e.target.value})
+    }
+
+    async handleSubmit(e) {
+        e.preventDefault();
+        // fetch(`http://localhost/api/administrator/find_administrator.php`, {
+        //     method: 'POST',
+        //     body: JSON.stringify({username:this.state.username, password: this.state.password})
+
+        const response = await fetch('http://localhost/api/administrator/find_administrator.php', {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({username: this.state.username, password: this.state.password}),
+        })
+        const data = await response.json()
+        if (data.length !== 0) {
+            sessionStorage.setItem('userData', JSON.stringify(data));
+            this.setState({
+                redirect: true
+            });
+            console.log(data)
+        } else {
+            this.setState({error: 'Incorrect username or password!'})
+        }
+    }
+
+    render() {
+        if (this.state.redirect) {
+            return (<Redirect to={'/admin'}/>)
+        }
+
+        if (sessionStorage.getItem('userData')) {
+            return (<Redirect to={'/admin'}/>)
+        }
+        return (
+            <React.Fragment>
+                <div className="limiter">
+                    <div className="container-login100">
+                        <div className="wrap-login100">
+                            <Tilt className="Tilt" options={{scale: 1.0}}>
+                                <div className="login100-pic js-tilt" data-tilt>
+                                    <img src={require("./images/img-01.png")} alt="IMG"/>
+                                </div>
+                            </Tilt>
+
+
+                            <form className="login100-form" onSubmit={this.handleSubmit.bind(this)}>
+                                <span className="login100-form-title">Member Login</span>
+
+                                <div className="wrap-input100">
+                                    <input
+                                        className="input100"
+                                        type="text"
+                                        name="username"
+                                        placeholder="Username"
+                                        onChange={this.handleChange.bind(this)}
+                                    />
+                                    <span className="focus-input100"></span>
+                                    <span className="symbol-input100">
+                    <i className="fa fa-envelope" aria-hidden="true"/>
                   </span>
-                </div>
+                                </div>
 
-                <div className="wrap-input100">
-                  <input
-                    className="input100"
-                    type="password"
-                    name="pass"
-                    placeholder="Password"
-                  />
-                  <span className="focus-input100"></span>
-                  <span className="symbol-input100">
-                    <i className="fa fa-lock" aria-hidden="true" />
+                                <div className="wrap-input100">
+                                    <input
+                                        className="input100"
+                                        type="password"
+                                        name="password"
+                                        placeholder="Password"
+                                        onChange={this.handleChange.bind(this)}
+                                    />
+                                    <span className="focus-input100"></span>
+                                    <span className="symbol-input100">
+                    <i className="fa fa-lock" aria-hidden="true"/>
                   </span>
-                </div>
+                                </div>
 
-                <div className="container-login100-form-btn">
-                  <button className="login100-form-btn">Login</button>
+                                <div className="container-login100-form-btn">
+                                    <p style={{color: 'red'}}>{this.state.error}</p>
+                                    <br/>
+                                    <button type="submit" className="login100-form-btn">Login</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
                 </div>
-
-                <div className="text-center p-t-12">
-                  <span className="txt1">Forgot</span>
-                  <a className="txt2" href="#">
-                    Username / Password?
-                  </a>
-                </div>
-
-                <div className="text-center p-t-136">
-                  <a className="txt2" href="#">
-                    Create your Account
-                    <i
-                      className="fa fa-long-arrow-right m-l-5"
-                      aria-hidden="true"
-                    ></i>
-                  </a>
-                </div>
-              </form>
-            </div>
-          </div>
-        </div>
-        <br />
-      </React.Fragment>
-    );
-  }
+                <br/>
+            </React.Fragment>
+        );
+    }
 }

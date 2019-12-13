ded2d37d172fb7e051e47d1dc357a35bde0804f1
Enes Alili <enesalili00@gmail.com>
2019-12-13 22:29:32 +0100

    adds edit component along with the route

--- src/App.js
+++ src/App.js
@@ -1,40 +1,42 @@
-import React, { Component } from 'react'
-import './App.css'
-import Home from './components/pages/homepage/Home'
-import { Switch, Route, BrowserRouter as Router } from 'react-router-dom'
-import NotFound from './components/pages/NotFound'
-import Header from './components/header/Header'
-import PhonesRoutes from './components/PhonesRoutes'
-import About from './components/pages/about/About'
-import Footer from './components/footer/Footer'
-import AddPhone from './components/admin/AddPhone'
-import Admin from './components/admin/Admin'
-import Login from './components/pages/login/Login'
-import Comparator from './components/pages/comparator/Comparator'
+import React, { Component } from "react";
+import "./App.css";
+import Home from "./components/pages/homepage/Home";
+import { Switch, Route, BrowserRouter as Router } from "react-router-dom";
+import NotFound from "./components/pages/NotFound";
+import Header from "./components/header/Header";
+import PhonesRoutes from "./components/PhonesRoutes";
+import About from "./components/pages/about/About";
+import Footer from "./components/footer/Footer";
+import AddPhone from "./components/admin/AddPhone";
+import Admin from "./components/admin/Admin";
+import Login from "./components/pages/login/Login";
+import Comparator from "./components/pages/comparator/Comparator";
+import Edit from "./components/admin/Edit";
 
 class App extends Component {
-  render () {
+  render() {
     return (
       <Router>
         <React.Fragment>
-          <Header/>
+          <Header />
           <section id="mainContent">
             <Switch>
-              <Route exact path="/" component={Home}/>
-              <Route path="/phones" component={PhonesRoutes}/>
-              <Route path="/about" component={About}/>
-              <Route path="/admin/addphone" component={AddPhone}/>
-              <Route path="/admin" component={Admin}/>
-              <Route path="/login" component={Login}/>
-              <Route path="/compare" component={Comparator}/>
-              <Route component={NotFound}/>
+              <Route exact path="/" component={Home} />
+              <Route path="/phones" component={PhonesRoutes} />
+              <Route path="/about" component={About} />
+              <Route path="/admin/addphone" component={AddPhone} />
+              <Route path="/admin/edit" component={Edit} />
+              <Route path="/admin" component={Admin} />
+              <Route path="/login" component={Login} />
+              <Route path="/compare" component={Comparator} />
+              <Route component={NotFound} />
             </Switch>
           </section>
-          <Footer/>
+          <Footer />
         </React.Fragment>
       </Router>
-    )
+    );
   }
 }
 
-export default App
+export default App;

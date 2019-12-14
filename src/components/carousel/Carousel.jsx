cf8f9fdb9ce1c579b46298c8af1bab82983c1a7b
Enes Alili <enesalili00@gmail.com>
2019-12-14 22:17:30 +0100

    changes carousel pics and adds prettier package

--- src/components/carousel/Carousel.jsx
+++ src/components/carousel/Carousel.jsx
@@ -1,6 +1,6 @@
-import React from 'react'
+import React from "react";
 
-export default function Carousel () {
+export default function Carousel() {
   return (
     <header>
       <div
@@ -14,48 +14,49 @@ export default function Carousel () {
             data-slide-to="0"
             className="active"
           />
-          <li data-target="#carouselExampleIndicators" data-slide-to="1"/>
-          <li data-target="#carouselExampleIndicators" data-slide-to="2"/>
+          <li data-target="#carouselExampleIndicators" data-slide-to="1" />
+          <li data-target="#carouselExampleIndicators" data-slide-to="2" />
         </ol>
         <div className="carousel-inner" role="listbox">
           <div
             className="carousel-item active"
             style={{
-              backgroundImage: `url(${require('../../half-sliders/iphone11pro.jpg')})`
+              backgroundImage: `url(${require("../../half-sliders/iphone-11-pro.jpg")})`
             }}
           >
             <div className="carousel-caption d-none d-md-block">
-              <h3>iPhone 11 Pro</h3>
-              <p>
-                Pro cameras. Pro display. Pro performance.{' '}
+              <h3 className="carouselItem">iPhone 11 Pro</h3>
+              <p className="carouselItem">
+                Pro cameras. Pro display. Pro performance.{" "}
               </p>
             </div>
           </div>
           <div
             className="carousel-item"
             style={{
-              backgroundImage: `url(${require('../../half-sliders/note10Carousel.jpg')})`
+              backgroundImage: `url(${require("../../half-sliders/samsung-galaxy-fold.png")})`
             }}
           >
             <div className="carousel-caption d-none d-md-block">
-              <h3>Next level power.</h3>
-              <p>
-                Experience that’s like a computer, a gaming console, a movie-tech camera,
-                and an intelligent pen, all in one device.
+              <h3 className="carouselItem">A device unlike any before.</h3>
+              <p className="carouselItem">
+                A new dawn in display technology: the foldable Infinity Flex
+                Display.
               </p>
             </div>
           </div>
           <div
             className="carousel-item"
             style={{
-              backgroundImage: `url(${require('../../half-sliders/mate20Pro.jpg')})`
+              backgroundImage: `url(${require("../../half-sliders/google-pixel-4.jpg")})`
             }}
           >
             <div className="carousel-caption d-none d-md-block">
-              <h3>Welcome to a Higher Intelligence</h3>
-              <p>
-                Limitless human imagination - that's the inspiration for the
-                Huawei Mate 20 Pro
+              <h3 className="carouselItem">
+                Capture the twinkle, twinkle just right.
+              </h3>
+              <p className="carouselItem">
+                Studio-like photos. Without the studio.
               </p>
             </div>
           </div>
@@ -66,7 +67,7 @@ export default function Carousel () {
           role="button"
           data-slide="prev"
         >
-          <span className="carousel-control-prev-icon" aria-hidden="true"/>
+          <span className="carousel-control-prev-icon" aria-hidden="true" />
           <span className="sr-only">Previous</span>
         </a>
         <a
@@ -75,10 +76,10 @@ export default function Carousel () {
           role="button"
           data-slide="next"
         >
-          <span className="carousel-control-next-icon" aria-hidden="true"/>
+          <span className="carousel-control-next-icon" aria-hidden="true" />
           <span className="sr-only">Next</span>
         </a>
       </div>
     </header>
-  )
+  );
 }

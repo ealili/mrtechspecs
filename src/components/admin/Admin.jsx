d068eda9bf0d8bd368c8f7d673ec1e110ee850ab
Selaudin Agolli <a.selaudin@gmail.com>
2019-12-25 16:17:27 +0100

    test

--- src/components/admin/Admin.jsx
+++ src/components/admin/Admin.jsx
@@ -7,59 +7,87 @@ class Admin extends Component {
     user: {}
   };
 
+
   componentWillMount() {
-    if (sessionStorage.getItem("userData")) {
+    if (sessionStorage.getItem("userData") ||localStorage.getItem("userData")) {
       console.log(sessionStorage.getItem("userData"));
     } else {
       this.setState({ redirect: true });
     }
   }
 
+  componentDidMount() {
+    this.CheckSession();
+  }
+
+  CheckSession() {
+    const response = fetch('http://localhost/api/retrievecookie.php');
+    const data = response.json();
+    if (data.length !== 0) {
+      this.setState({
+        redirect: true
+      })
+    console.log("SESSION IS HERE")
+    }
+      else
+      {
+        console.log("no session");
+      }
+
+  }
+
+
   logout() {
+    fetch('http://localhost/api/removecookie.php')
+        .then(response => response.json());
     sessionStorage.setItem("userData", "");
+    localStorage.setItem("userData", "");
     sessionStorage.clear();
-    this.setState({ redirect: true });
+    localStorage.clear();
+    this.setState({redirect: true});
     console.log(sessionStorage.getItem("userData"));
   }
 
   render() {
     if (this.state.redirect) {
-      return <Redirect to={"/login"} />;
+      return <Redirect to={"/login"}/>;
     }
-    return (
-      <div className="container">
-        <hr />
-        <div className="row text-center text-lg-left">
-          <div className="col-lg-3 col-md-4 col-xs-6">
-            <Link to="/admin/add-phone" className="nav-link">
-              <i class="fa fa-plus-circle" style={this.iconStyles}>
-                <div style={{ fontSize: "25px" }}>Add Phone</div>
-              </i>
-            </Link>
+    else/*if(sessionStorage.getItem('userData') || localStorage.getItem('userData')*/ {
+      return (
+          <div className="container">
+            <hr/>
+            <div className="row text-center text-lg-left">
+              <div className="col-lg-3 col-md-4 col-xs-6">
+                <Link to="/admin/add-phone" className="nav-link">
+                  <i class="fa fa-plus-circle" style={this.iconStyles}>
+                    <div style={{fontSize: "25px"}}>Add Phone</div>
+                  </i>
+                </Link>
+              </div>
+              <div className="col-lg-3 col-md-4 col-xs-6">
+                <Link to="/admin/edit-phone" className="nav-link">
+                  <i class="fa fa-edit" style={this.iconStyles}>
+                    <div style={{fontSize: "25px"}}>Edit Phone</div>
+                  </i>
+                </Link>
+              </div>
+              <div className="col-lg-3 col-md-4 col-xs-6">
+                <a
+                    href=""
+                    onClick={this.logout.bind(this)}
+                    className="logout nav-link"
+                >
+                  <i class="fa fa-sign-out" style={this.iconStyles}>
+                    <div style={{fontSize: "25px", color: "#0000FF"}}>
+                      Sign Out
+                    </div>
+                  </i>
+                </a>
+              </div>
+            </div>
           </div>
-          <div className="col-lg-3 col-md-4 col-xs-6">
-            <Link to="/admin/edit-phone" className="nav-link">
-              <i class="fa fa-edit" style={this.iconStyles}>
-                <div style={{ fontSize: "25px" }}>Edit Phone</div>
-              </i>
-            </Link>
-          </div>
-          <div className="col-lg-3 col-md-4 col-xs-6">
-            <a
-              href=""
-              onClick={this.logout.bind(this)}
-              className="logout nav-link"
-            >
-              <i class="fa fa-sign-out" style={this.iconStyles}>
-                <div style={{ fontSize: "25px", color: "#0000FF" }}>
-                  Sign Out
-                </div>
-              </i>
-            </a>
-          </div>
-        </div>
-      </div>
-    );
+      );
+    }
   }
 
   iconStyles = {

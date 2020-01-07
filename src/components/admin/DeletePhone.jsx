93c40bd5da7e88bcb512898fce2f45201ae8229d
Enes Alili <enesalili00@gmaili.com>
2020-01-07 11:23:30 +0300

    added some functionality to delete phone

--- src/components/admin/DeletePhone.jsx
+++ src/components/admin/DeletePhone.jsx
@@ -1,13 +1,40 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
+import DeleteThumbnail from "./DeleteThumbnail";
 
-class DeletePhone extends Component {
-    render() {
-        return (
-            <div>
-                Delete Phone Component
+export default class DeletePhone extends Component {
+  state = {
+    loading: true,
+    latestPhones: []
+  };
+  async componentDidMount() {
+    const url = "http://localhost/api/phone/get_all_phones.php";
+    const response = await fetch(url);
+    const data = await response.json();
+    this.setState({ latestPhones: data, loading: false });
+  }
+
+  render() {
+    if (this.state.loading) {
+      return <div className="lds-hourglass"></div>;
+    } else {
+      return (
+        <section className="py-5">
+          <div className="container">
+            <div className="row text-center text-lg-left">
+              <h3 className="my-4 text-center text-lg-left">
+                Showing All Phones
+              </h3>
+              <div className="container">
+                <div className="row text-center text-lg-left">
+                  {this.state.latestPhones.map(latestPhone => (
+                    <DeleteThumbnail key={latestPhone.id} phone={latestPhone} />
+                  ))}
+                </div>
+              </div>
             </div>
-        );
+          </div>
+        </section>
+      );
     }
+  }
 }
-
-export default DeletePhone;
